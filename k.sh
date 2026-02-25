d4e185779afb0d687e770d672ba4e66d11b5e2d6
Felipe Silva <feeelipe.silva@gmail.com>
2026-02-25 13:47:09 -0300

    Remove vendor/install artifacts and local scripts

--- k.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-export AWS_PROFILE=ec2admin
-export AWS_REGION=us-east-2
-export AWS_DEFAULT_REGION=us-east-2
-kubectl "$@"
--- tf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-export AWS_PROFILE=infra
-export AWS_REGION=us-east-1
-export AWS_DEFAULT_REGION=us-east-1
-terraform "$@"
