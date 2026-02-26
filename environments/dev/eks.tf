0962bef57a59e686ff457d19956694f5685d38c9
Felipe Silva <feeelipe.silva@gmail.com>
2026-02-26 08:15:59 -0300

    feat: ajustado nome de maquina

--- environments/dev/eks.tf
+++ environments/dev/eks.tf
@@ -51,7 +51,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      name           = "default"
+      name           = "eks-machine"
       instance_types = ["t3.small"]
 
       min_size     = 1
