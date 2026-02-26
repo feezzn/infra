0c0d47ec6ebe1c70adcd6aefebba406458dc4482
Felipe Silva <feeelipe.silva@gmail.com>
2026-02-26 18:55:20 -0300

    feat: adding addons and lb

--- environments/dev/addons.tf
+++ environments/dev/addons.tf
@@ -2,7 +2,7 @@ module "eks_addons" {
   source = "../../modules/eks-addons"
 
   cluster_name      = module.eks.cluster_name
-  region            = var.region        # ou var.aws_region
+  region            = var.region # ou var.aws_region
   vpc_id            = module.vpc.vpc_id
   oidc_provider_arn = module.eks.oidc_provider_arn
   oidc_issuer_url   = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
