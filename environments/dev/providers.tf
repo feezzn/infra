526d25d994a58d13a0e8f67ab573d110336f3c7c
Felipe Silva <felipe.sisilva@bancobmg.com.br>
2026-03-16 15:28:52 -0300

    fix: resolve lint and tflint compatibility issues

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -1,21 +1,23 @@
-data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
+# EKS Cluster Data + Kubernetes/Helm Providers (COMMENTED)
+# Uncomment after EKS cluster is provisioned in eks.tf
+# data "aws_eks_cluster" "this" {
+#   name = module.eks.cluster_name
+# }
+#
+# data "aws_eks_cluster_auth" "this" {
+#   name = module.eks.cluster_name
+# }
+#
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.this.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.this.token
+# }
+#
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.this.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.this.token
+#   }
+# }
