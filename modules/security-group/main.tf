b55a6c833e8d4c301997d7acc4cd5253496fcb66
Felipe Silva <felipe.sisilva@bancobmg.com.br>
2026-03-16 16:07:04 -0300

    fix: resolve lint and tflint compatibility issues

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "this" {
   name        = var.name
-  description = "Managed by Terraform"
+  description = "Security group for ${var.name}"
   vpc_id      = var.vpc_id
 
   dynamic "ingress" {
